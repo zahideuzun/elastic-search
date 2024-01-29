06a16940629d7eb7d21d7cb907b924191100ca50
zahideuzunn <zahide.uzun@renault.com.tr>
2024-01-30 00:38:42 +0300

    models and product repository created

---
+++ API/ElasticSearch/ElasticSearch.API/Models/ProductFeature.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ElasticSearch.API.Models
+{
+    public class ProductFeature
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public EColor Color { get; set; }
+    }
+}
---
+++ API/ElasticSearch/ElasticSearch.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using ElasticSearch.API.Models;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearch.DAL.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _client;
+
+        public ProductRepository(ElasticClient client)
+        {
+            _client = client;
+        }
+        
+        public async Task<Product?> SaveAsync(Product newProduct)
+        {
+            newProduct.Created = DateTime.Now;
+
+            var response = await _client.IndexAsync(newProduct, x => x.Index("products"));
+
+            if (!response.IsValid) return null;
+
+            newProduct.Id = response.Id;
+            
+            return newProduct;
+        }
+
+    }
+}
