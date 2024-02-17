66d93df9aedd23872bfbf4fb168dd11518175f48
zahideuzunn <zahide.uzun@renault.com.tr>
2024-02-18 01:30:52 +0300

    eCommerce ui tamamlandi.

--- API/ElasticSearch/ElasticSearch.AppCore/DTOs/ECommerceDTOs/ECommerceSearchDto.cs
+++ API/ElasticSearch/ElasticSearch.AppCore/DTOs/ECommerceDTOs/ECommerceSearchDto.cs
@@ -1,6 +1,8 @@
 ﻿using Nest;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +11,23 @@ namespace ElasticSearch.AppCore.DTOs.ECommerceDTOs
 {
 	public class ECommerceSearchDto
 	{
-		
-		public string? CustomerFullName { get; set; } 
 
+		[DisplayName("Category")]
 		public string? Category { get; set; }
 
-		public string? Gender { get; set; } 
+		[DisplayName("Gender")]
+		public string? Gender { get; set; }
 
+		[DisplayName("Order Start Date")]
+		[DataType(DataType.Date)]
 		public DateTime? OrderDateStart { get; set; }
 
+		[DisplayName("Order End Date")]
+		[DataType(DataType.Date)]
 		public DateTime? OrderDateEnd { get; set; }
+
+		[DisplayName("Customer Full Name")]
+		public string? CustomerFullName { get; set; }
 
 	}
 }
